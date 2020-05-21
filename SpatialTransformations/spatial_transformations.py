a51033b32422981e494f699eb8e66f089e65020f
John Oyster <25227550+JohnOyster@users.noreply.github.com>
2020-05-20 22:47:15 -0400

    Added Gamma Correction Function

--- SpatialTransformations/spatial_transformations.py
+++ SpatialTransformations/spatial_transformations.py
@@ -43,7 +43,7 @@ def negative_transform(image):
     :rtype:         numpay.ndarray
     """
     L = 2 ** int(round(np.log2(image.max())))       # Input gray level
-    new_image = (L - 1) - image                         # s = L - 1 - r
+    new_image = (L - 1) - image                     # s = L - 1 - r
     new_image = new_image.astype(np.uint8)
 
     return new_image
@@ -57,11 +57,32 @@ def log_transform(image):
     :return:        log transformed image file
     :rtype:         numpy.ndarray
     """
-    c = 255 / np.log(1 + np.max(image))
+    c = np.iinfo(image.dtype).max / np.log(1 + np.max(image))       # Selecting c to help normalize r
     new_image = c * np.log(1 + image)
     new_image = new_image.astype(np.uint8)
 
     return new_image
+
+
+def gamma_transformation(image, gamma=1.0):
+    """Power-Law (Gamma) Transformation of grayscale image.
+
+    :param image:   Original Image
+    :type:          numpy.ndarray
+    :param gamma:   Gamma value to apply
+    :type:          float
+    :return:        gamma transformed image file
+    :rtype:         numpy.ndarray
+    """
+    c = 255
+    norm_image = image / np.max(image)
+    new_image = c * np.power(norm_image, gamma)
+    new_image = new_image.astype(np.uint8)
+
+    return new_image
+
+
+def histogram
 
 if __name__ == '__main__':
     image_file = cv2.imread("./Cleveland.jpg")
@@ -75,5 +96,8 @@ if __name__ == '__main__':
     cv2.waitKey(0)
     log_image = log_transform(gray_image)
     cv2.imshow('Logarithmic Transformed Image', log_image)
+    cv2.waitKey(0)
+    gamma_image = gamma_transformation(gray_image, 2.0)
+    cv2.imshow('Gamma Correction', gamma_image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
